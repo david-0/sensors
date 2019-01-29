9965c98518504fe415fe10bc6ecce970952ac057
David Leuenberger <david.leuenberger@hsr.ch>
2019-01-29 23:09:49 +0100

    add logs

--- sensors-backend/src/main/java/org/sensors/backend/App.java
+++ sensors-backend/src/main/java/org/sensors/backend/App.java
@@ -5,21 +5,26 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 
 public class App {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
 	public static void main(String[] args)
 			throws UnsupportedBusNumberException, IOException, InterruptedException, ExecutionException {
 		I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
 		SensorMcp9808 sensor1 = new SensorMcp9808(bus, 0x18, "Controller");
 		SensorMcp9808 sensor2 = new SensorMcp9808(bus, 0x1C, "Unbekannt");
 
+		logger.info("start init ...");
 		StopWatch watch = StopWatch.createStarted();
 		AsyncCombiner.allOf(sensor1::init, sensor2::init).get();
 		watch.stop();
+		logger.info("... init finished");
 		System.out.printf("Init duration: %d ms %n", watch.getTime(TimeUnit.MILLISECONDS));
 		
 
