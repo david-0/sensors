ed89c957b30201422b2524721bef77d2cfca2a7c
David Leuenberger <david.leuenberger@hsr.ch>
2019-01-30 10:50:15 +0100

    serializer fixed

--- sensors-backend/src/main/java/org/sensors/backend/Controller.java
+++ sensors-backend/src/main/java/org/sensors/backend/Controller.java
@@ -52,8 +52,8 @@ public class Controller {
 		props.put("batch.size", 16384);
 		props.put("linger.ms", 1);
 		props.put("buffer.memory", 33554432);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.FloatSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.FloatSerializer");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		return props;
 	}
 }
