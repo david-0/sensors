1a31e9fb8b90c0fcb4a7e1345cc1f5f5faee02ed
David Leuenberger <daivd.leuenberger@gmx.ch>
2019-02-06 08:13:41 +0100

    OneWire test

--- testOneWire/src/main/java/org/myprojects/testOneWire/App.java
+++ testOneWire/src/main/java/org/myprojects/testOneWire/App.java
@@ -1,28 +1,31 @@
 package org.myprojects.testWs2811B;
 
 import java.io.IOException;
+import java.util.List;
 
-import com.github.mbelling.ws281x.Color;
-import com.github.mbelling.ws281x.LedStripType;
-import com.github.mbelling.ws281x.Ws281xLedStrip;
+import com.pi4j.component.temperature.TemperatureSensor;
+import com.pi4j.component.temperature.impl.TmpDS18B20DeviceType;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
+import com.pi4j.io.w1.W1Device;
+import com.pi4j.io.w1.W1Master;
 
 public class App 
 {
 	public static void main(String[] args) throws UnsupportedBusNumberException, IOException, InterruptedException
     {
-		Ws281xLedStrip ws281xLedStrip = new Ws281xLedStrip(8, 18, 800000, 10, 255, 0, false, LedStripType.WS2811_STRIP_GBR, true);
-		ws281xLedStrip.setStrip(Color.GREEN);
-		ws281xLedStrip.render();
-		Thread.sleep(5000);
-		ws281xLedStrip.setStrip(Color.WHITE);
-		ws281xLedStrip.render();
-		Thread.sleep(5000);
-		ws281xLedStrip.setStrip(Color.RED);
-		ws281xLedStrip.render();
-		Thread.sleep(5000);
-		ws281xLedStrip.setStrip(Color.BLUE);
-		ws281xLedStrip.render();
-		Thread.sleep(5000);
+		W1Master master = new W1Master();
+		List<W1Device> w1Devices = master.getDevices(TmpDS18B20DeviceType.FAMILY_CODE);
+		for (W1Device device : w1Devices) {
+		    //this line is enought if you want to read the temperature
+		    System.out.println("Temperature: " + ((TemperatureSensor) device).getTemperature());
+		    //returns the temperature as double rounded to one decimal place after the point
+
+		    try {
+		        System.out.println("1-Wire ID: " + device.getId() +  " value: " + device.getValue());
+		        //returns the ID of the Sensor and the  full text of the virtual file
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		}
     }
 }
