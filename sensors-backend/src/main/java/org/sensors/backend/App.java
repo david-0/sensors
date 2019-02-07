2bb22502004b1f7d78741c2a80995c567c5ddf76
David Leuenberger <david.leuenberger@hsr.ch>
2019-02-07 07:33:51 +0100

    fix wlanOn state

--- sensors-backend/src/main/java/org/sensors/backend/App.java
+++ sensors-backend/src/main/java/org/sensors/backend/App.java
@@ -21,6 +21,7 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
@@ -116,7 +117,9 @@ public class App {
 					GpioPinDigitalStateChangeEvent event) {
 				System.out.println(" --> GPIO PIN (WLAN Button) STATE CHANGE: "
 						+ event.getPin() + " = " + event.getState());
-				wlanOn = !wlanOn;
+				if (event.getState().equals(PinState.LOW)) {
+					wlanOn = !wlanOn;
+				}
 				wlanLED.setState(wlanOn);
 			}
 
