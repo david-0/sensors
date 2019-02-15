64ab2ac489fc338b59142edbd9997dac7fe45f9e
David Leuenberger <daivd.leuenberger@gmx.ch>
2019-02-15 20:51:35 +0100

    update start script

--- startKafka.sh
+++ startKafka.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-nohup ../kafka_2.11-2.1.0/bin/zookeeper-server-start.sh ../kafka_2.11-2.1.0/config/zookeeper.properties 2>&1 > logs/zookeeper.log < /dev/zero &
-nohup ../kafka_2.11-2.1.0/bin/kafka-server-start.sh ../kafka_2.11-2.1.0/config/server.properties 2>&1 > logs/server.log < /dev/zero &
+nohup ../kafka_2.11-2.1.0/bin/zookeeper-server-start.sh ../kafka_2.11-2.1.0/config/zookeeper.properties 2>&1 > logs/zookeeper-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
+nohup ../kafka_2.11-2.1.0/bin/kafka-server-start.sh ../kafka_2.11-2.1.0/config/server.properties 2>&1 > logs/server-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
 
-sudo nohup java -jar sensors-backend/target/sensors-backend-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 > logs/backend.log < /dev/zero &
+sudo nohup java -jar sensors-backend/target/sensors-backend-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 > logs/backend-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
