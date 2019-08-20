9de57c78adc5d2ecebd6e43953d19314d13c3cde
David Leuenberger <daivd.leuenberger@gmx.ch>
2019-08-20 21:56:26 +0200

    changed start script

--- startKafka.sh
+++ startKafka.sh
@@ -2,7 +2,10 @@
 
 cd "$(dirname "$0")"
 
-nohup ../kafka_2.11-2.1.0/bin/zookeeper-server-start.sh ../kafka_2.11-2.1.0/config/zookeeper.properties 2>&1 > logs/zookeeper-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
-nohup ../kafka_2.11-2.1.0/bin/kafka-server-start.sh ../kafka_2.11-2.1.0/config/server.properties 2>&1 > logs/server-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
+d=$(date +%Y%m%d-%H%M%S)
 
-sudo nohup java -jar sensors-backend/target/sensors-backend-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 > logs/backend-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
+nohup ../kafka_2.11-2.1.0/bin/zookeeper-server-start.sh ../kafka_2.11-2.1.0/config/zookeeper.properties 2>&1 > logs/zookeeper-${d}.log < /dev/zero &
+nohup ../kafka_2.11-2.1.0/bin/kafka-server-start.sh ../kafka_2.11-2.1.0/config/server.properties 2>&1 > logs/server-${d}.log < /dev/zero &
+
+sudo nohup java -jar sensors-backend/target/sensors-backend-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 > logs/backend-${d}.log < /dev/zero &
+sudo nohup java -jar sensors-logic/target/sensors-logic-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 > logs/logic-${d}.log < /dev/zero &
