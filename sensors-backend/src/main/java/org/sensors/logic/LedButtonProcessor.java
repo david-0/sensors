4e2da015264c53d954b94d6150feabb238e2ce81
David <david.leuenberger@gmx.ch>
2020-07-08 20:12:44 +0200

    fix import

--- sensors-backend/src/main/java/org/sensors/logic/LedButtonProcessor.java
+++ sensors-backend/src/main/java/org/sensors/logic/LedButtonProcessor.java
@@ -1,7 +1,6 @@
 package org.sensors.logic;
 
 import java.time.ZonedDateTime;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
