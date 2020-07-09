4fa8cd4e8c77a4d61730fdbcd5090540eb175812
David <david.leuenberger@gmx.ch>
2020-07-09 17:22:13 +0200

    Refactoring

---
+++ sensors-backend/src/main/java/org/sensors/backend/SensorApp.java
@@ -0,0 +1,98 @@
+package org.sensors.backend;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.sensors.backend.device.Button;
+import org.sensors.backend.device.DigialOutputDevice;
+import org.sensors.backend.device.LedStrip;
+import org.sensors.backend.device.SensorBME280;
+import org.sensors.backend.device.SensorMcp9808;
+import org.sensors.backend.device.SensorMe2O2;
+import org.sensors.backend.device.SensorOneWireTemp;
+import org.sensors.backend.device.SensorSHT31d;
+import org.sensors.backend.device.WlanControlOutputDevice;
+import org.sensors.backend.device.ina219.SensorIna219;
+import org.sensors.logic.LedButtonProcessor;
+import org.sensors.logic.WlanButtonProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CFactory;
+import com.pi4j.io.w1.W1Master;
+
+public class SensorApp {
+	private static final Logger logger = LoggerFactory.getLogger(SensorApp.class);
+	private SensorController sensorController;
+
+	public void init(StateStore stateStore) {
+		try {
+			final I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
+			final W1Master master = new W1Master();
+			final GpioController gpio = GpioFactory.getInstance();
+
+			LedStrip ledStripSchrank = new LedStrip(8).init();
+			Button ledButton = new Button(gpio, RaspiPin.GPIO_27, true).init();
+			Button wlanButton = new Button(gpio, RaspiPin.GPIO_24, false).init();
+			DigialOutputDevice wlanButtonLed = new DigialOutputDevice(gpio, RaspiPin.GPIO_25).init();
+			WlanControlOutputDevice wlanOutputDevice = new WlanControlOutputDevice();
+
+			LedButtonProcessor ledButtonProcessor = new LedButtonProcessor(ledStripSchrank);
+			WlanButtonProcessor wlanButtonProcessor = new WlanButtonProcessor(wlanButtonLed, wlanOutputDevice);
+
+			ledButton.onChange(ledButtonProcessor::update);
+			wlanButton.onChange(wlanButtonProcessor::update);
+
+			sensorController = new SensorController((id, value) -> stateStore.update(id, value));
+			createMcp9808Sensors(bus).stream().forEach(sensorController::addIntervalBasedSource);
+			createIna219Sensors(bus).stream().forEach(sensorController::addIntervalBasedSource);
+			createOneWireSensors(master).stream().forEach(sensorController::addIntervalBasedSource);
+
+			sensorController.addIntervalBasedSource(new SensorMe2O2(bus, 0x04, "o2", "O2 Sensor", 84.0).init());
+			sensorController
+					.addIntervalBasedSource(new SensorSHT31d(bus, 0x44, "sht31d", "temp / humidity Sensor").init());
+			sensorController.addIntervalBasedSource(
+					new SensorBME280(bus, 0x77, "bme280", "temp / humidity / pressure Sensor").init());
+			sensorController.init();
+		} catch (Exception e) {
+			logger.error("Abort main", e);
+			System.exit(1);
+		}
+	}
+
+	public void start() {
+		sensorController.run();
+		logger.info("controller started");
+	}
+
+	public void stop() {
+		logger.info("controller stopping");
+		sensorController.stop();
+		sensorController.waitMainThread();
+		logger.info("controller stopped");
+	}
+
+	private static List<SensorMcp9808> createMcp9808Sensors(I2CBus bus) {
+		return Arrays.asList(//
+				new SensorMcp9808(bus, 0x18, "controller", "Temp Controller").init(),
+				new SensorMcp9808(bus, 0x1C, "unknown", "Temp Unbekannt").init());
+	}
+
+	private static List<SensorIna219> createIna219Sensors(I2CBus bus) {
+		return Arrays.asList(//
+				new SensorIna219(bus, 0x40, "ina219-led", "INA219 1 - LED").init(),
+				new SensorIna219(bus, 0x41, "ina219-raspi", "INA219 2 - Raspi").init(),
+				new SensorIna219(bus, 0x45, "ina219-input", "INA219 3 - Input").init());
+	}
+
+	private static List<SensorOneWireTemp> createOneWireSensors(W1Master master) {
+		return Arrays.asList(//
+				new SensorOneWireTemp(master, "28-0000046d50e7", "T1-aussen", "Abwassertank aussen").init(),
+				new SensorOneWireTemp(master, "28-0000093001f5", "T2-luft", "Aussentemperatur").init(),
+				new SensorOneWireTemp(master, "28-00000a25c18f", "T3-innen", "Abwassertank innen").init());
+	}
+}
